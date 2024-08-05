8d01027f5742db16dff4eaac504abd39c7e1d5a1
Masahiko Sawada <msawada@postgresql.org>
2024-08-05 06:05:23 -0700

    Restrict accesses to non-system views and foreign tables during pg_dump.
    When pg_dump retrieves the list of database objects and performs the
    data dump, there was possibility that objects are replaced with others
    of the same name, such as views, and access them. This vulnerability
    could result in code execution with superuser privileges during the
    pg_dump process.
    
    This issue can arise when dumping data of sequences, foreign
    tables (only 13 or later), or tables registered with a WHERE clause in
    the extension configuration table.
    
    To address this, pg_dump now utilizes the newly introduced
    restrict_nonsystem_relation_kind GUC parameter to restrict the
    accesses to non-system views and foreign tables during the dump
    process. This new GUC parameter is added to back branches too, but
    these changes do not require cluster recreation.
    
    Back-patch to all supported branches.
    
    Reviewed-by: Noah Misch
    Security: CVE-2024-7348
    Backpatch-through: 12

--- src/test/regress/sql/create_view.sql
+++ src/test/regress/sql/create_view.sql
@@ -714,6 +714,16 @@ CREATE RULE "_RETURN" AS ON SELECT TO tt28 DO INSTEAD SELECT * FROM tt26;
 CREATE RULE "_RETURN" AS ON SELECT TO tt28 DO INSTEAD SELECT * FROM tt26;
 ROLLBACK;
 
+
+-- test restriction on non-system view expansion.
+create table tt27v_tbl (a int);
+create view tt27v as select a from tt27v_tbl;
+set restrict_nonsystem_relation_kind to 'view';
+select a from tt27v where a > 0; -- Error
+insert into tt27v values (1); -- Error
+select viewname from pg_views where viewname = 'tt27v'; -- Ok to access a system view.
+reset restrict_nonsystem_relation_kind;
+
 -- test display negative operator of const-folder expression
 create table tdis(a int, b int, c int);
 create view tdis_v1 as select a,b,c, -1::int from tdis group by 1,2,3,4;
